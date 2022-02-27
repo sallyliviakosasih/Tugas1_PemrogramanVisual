a05b59b1e633c901abaf090081c2607fb8d3567a
sallyliviakosasih <86108506+sallyliviakosasih@users.noreply.github.com>
2022-02-27 12:19:45 +0700

    Add files via upload

---
+++ tugas1.2/tugas02/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sally
+ * Date: 2/25/2022
+ * Time: 9:06 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BelajarCSHARP
+{
+	class programHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Nama kamu siapa? ");
+			string nama = Console.ReadLine();
+			Console.WriteLine("Nama Kamu "+nama);
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
