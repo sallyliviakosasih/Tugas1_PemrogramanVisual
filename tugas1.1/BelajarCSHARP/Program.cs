d198e91cca31705f8dfa5fa996a97a2f0ce75cb7
sallyliviakosasih <86108506+sallyliviakosasih@users.noreply.github.com>
2022-02-27 12:17:51 +0700

    Add files via upload

---
+++ tugas1.1/BelajarCSHARP/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sally
+ * Date: 2/25/2022
+ * Time: 7:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BelajarCSHARP
+{
+	class programHW
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey(true);
+			
+		}
+	}
+}
